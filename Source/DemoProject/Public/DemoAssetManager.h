cb2bbbc76d1eaaec427ecf8096cf0a9737dc0157
Adamz <adam.zikry23@gmail.com>
2024-12-07 17:40:30 +0800

    Initial Commit

---
+++ Source/DemoProject/Public/DemoAssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "DemoAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMOPROJECT_API UDemoAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
